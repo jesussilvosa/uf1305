a92ab1a2fd945fd5ce85c5919b9e02160e34c08a
jesussilvosa <95342278+jesussilvosa@users.noreply.github.com>
2022-03-22 09:32:49 +0100

    changes 02

--- js/calculos.js
+++ js/calculos.js
@@ -16,25 +16,25 @@ function mostrarMultiplo(n, m) {
 }
 
 
-function mostrarFactorial(nf) {
-    document.getElementById('td21').innerHTML = nf;
-    document.getElementById('td22').innerHTML = factorial(nf);
+function mostrarFactorial( n ) {
+    document.getElementById('td21').innerHTML = n;
+    document.getElementById('td22').innerHTML = factorial( n );
 }
 
 
-function mostrarResultados(op, n1, n2 = 0) {
+function mostrarResultados( op, n, m = 0 ) {
 
     switch (op) {
         case 'multiplo':
-            document.getElementById('td11').innerHTML = n1;
-            document.getElementById('td12').innerHTML = n2;
-            document.getElementById('td13').innerHTML = multiplosDeHasta(n1, n2);
-            break;
+            document.getElementById('td11').innerHTML = n;
+            document.getElementById('td12').innerHTML = m;
+            document.getElementById('td13').innerHTML = multiplosDeHasta(n, m);
+        break;
 
-        case 'factorial':
-            document.getElementById('td21').innerHTML = n1;
-            document.getElementById('td22').innerHTML = factorial(n1);
-            break;
+        case 'factorial' :
+            document.getElementById('td21').innerHTML = n;
+            document.getElementById('td22').innerHTML = factorial( n );
+        break;
         default:
     }
 }
@@ -48,33 +48,44 @@ function multiplosDeHasta(n, m) {
         " son: ";
 
     multiplo = 0;
-    for (i = 1; multiplo < m; i++) {
-        multiplo = n * i;
-        mensaje = mensaje + multiplo + " ";
-    }
 
+    // for (i = 1; multiplo < m; i++) {
+    //     multiplo = n * i;
+    //     mensaje = mensaje + multiplo + " ";
+    // }
+
+    // Inicialización de la variable contador
     i = 1;
 
-    while (multiplo < m;) {
+    // Condición
+    while ( multiplo < m ) {
         multiplo = n * i;
         mensaje = mensaje + multiplo + " ";
 
-        
+        // Incremento
         i++;
     }
-        return mensaje;
+
+
+
+    return mensaje;
+}
+
+function factorial( n ) {
+    var factorial = 1;
+    for ( i=1; i <= n; i++ ) {
+        factorial = factorial * i;
     }
+    return factorial;
+}
+
+function sumar( s1, s2 ) {
+    var suma = s1 + s2;
+    return suma;
+}
 
 
-    function factorial(nf) {
-        var factorial = 1;
-        for (i = 1; i <= nf; i++) {
-            factorial = factorial * i;
-        }
-        return factorial;
-    }
-
-
-
-
+function multiplicar( f1, f2 ){
+    var producto = f1 * f2;
+    return producto;
 }
